db50ce517d27f8d6dd277e3812068512effaa20d
mpshabeer <mpshabeer8182@gmail.com>
2023-06-18 19:09:22 +0530

    second commit

--- Music_sharing_portal/settings.py
+++ Music_sharing_portal/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@ekgw63s@z!4_-4b((o5$0jux2b1m=wwn+e*33gfrv0f^j9ill'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
